503e0ff3d517753a02b07ec9067ce25c1ac06d62
bufleek <brunookiprop@gmail.com>
2019-10-31 23:02:40 +0300

    problem 17 solved....falsey Number

--- 1-basics/basics.js
+++ 1-basics/basics.js
@@ -285,7 +285,9 @@ describe("basics", function() {
             ////////////////////////////////////////////////////////////////////
             // Can you find two different numbers that each cast to `false` in an if() statement?
             // ✏️ ADD CODE HERE ✏️
-
+            const falsyNumber1 = 0;
+            const falsyNumber2 = NaN;
+        
             ////////////////////////////////////////////////////////////////////
 
             expect(typeof falsyNumber1).to.equal("number");
